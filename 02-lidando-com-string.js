d95ef546dea2978e135ffc6da105b223b540fed9
mayconr4 <mayconhs2007@gmail.com>
2025-06-05 11:57:47 -0300

    feito exemplo de concatenaçãp

---
+++ 02-lidando-com-string.js
@@ -0,0 +1,17 @@
+// 02-lidando-com-strings.js
+'use strict';
+
+const nome = "Chapolin";
+const sobrenome = "Colorado";
+let idade = 20;
+let cidade  =  "São Paulo";
+let estado = "SP";
+
+
+// Meu nome é chapolin colorado, tenho 20 anos e moro em SÃO paulo/sp. 
+
+
+//CONCATENÇÃO tradiciona usa o ooperador +
+let mensagem1 = "Meu nome é "+nome+" "+sobrenome+" tenho "+idade+" anos e moro em "+cidade+"/"+estado+".";
+
+//TEMPLATE STRING/LITERAL
