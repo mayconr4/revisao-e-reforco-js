dc0b9819281e8d4a9aef041a052ca3217411e23f
mayconr4 <mayconhs2007@gmail.com>
2025-06-09 09:38:32 -0300

    refazendo função separador para organizar a saida do código

--- 05-usando-modulos.js
+++ 05-usando-modulos.js
@@ -1,7 +1,12 @@
 'use strict';
 
+// Importação de recurso (padrão default) 
+import alunos from "./modulos-esm/alunos.js";
+
 //Iportação de multiplos recursos/módulos usando ESM
-import { saudacao,clientes } from "./modulos-esm/exemplos.js"; 
+import { saudacao,clientes, separador } from "./modulos-esm/exemplos.js"; 
 
 saudacao("Fulano de tal");
 console.log(clientes);
+
+separador();
--- modulos-esm/exemplos.js
+++ modulos-esm/exemplos.js
@@ -8,4 +8,11 @@ function saudacao(nome){
 
 const clientes = ["Maycon","Gabriel","Júlia"];
 
-export{saudacao, clientes};
+function separador(){
+    console.log("\n********************************\n");
+}
+
+
+export{saudacao, clientes, separador}; 
+
+
