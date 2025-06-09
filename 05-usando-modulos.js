55b67dd11343056bbab08c2db2cd41c6cf17e569
mayconr4 <mayconhs2007@gmail.com>
2025-06-09 10:02:26 -0300

    feito teste de impor

--- 05-usando-modulos.js
+++ 05-usando-modulos.js
@@ -1,7 +1,10 @@
 'use strict';
 
 // Importação de recurso (padrão default) 
-import alunos from "./modulos-esm/alunos.js";
+import alunos from "./modulos-esm/alunos.js"; 
+
+// osu do as/apelido para veitar conflito de nomes
+import {pessoa,livro,alunos as personagens} from "./dados.js";
 
 //Iportação de multiplos recursos/módulos usando ESM
 import { saudacao,clientes, separador } from "./modulos-esm/exemplos.js"; 
@@ -21,4 +24,9 @@ for (const aluno of alunos){
     
     console.log(aluno); 
 
-}
+} 
+
+separador();
+
+console.log("Personagens");
+console.log(personagens);
---
+++ dados.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+const pessoa = { 
+    nome: 'Ian gilian',
+    idade: 78,
+    cidade: 'SÂo Paulo'
+
+};
+
+const livro = { 
+    titulo: 'O senhor dos aníes ',
+    volume: 'As duads torres',
+    ano: 1954
+
+};
+
+
+const alunos = ['Pica Pau ', 'Zé colmeia ', 'Naruto', 'luffy'];
+
+export{pessoa,livro,alunos};
