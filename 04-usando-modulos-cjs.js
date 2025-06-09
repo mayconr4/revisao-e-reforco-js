dbcab7269db7b5515bd3ee365c767aec169f1138
mayconr4 <mayconhs2007@gmail.com>
2025-06-09 09:06:13 -0300

    teste criando funcao em modulo

---
+++ 04-usando-modulos-cjs.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+//Importando os mudulos usando CJS  e require 
+const moduloExemplos = require('./modulos-cjs/exemplos.js');
+
+
+//Chmando a função existente no modulo
+moduloExemplos.minhaFuncao();
+
+console.log(moduloExemplos.clientes);
