22bbabfd662010292abc8f3624d335b9beff037a
mayconr4 <mayconhs2007@gmail.com>
2025-06-09 09:11:43 -0300

    usado outro tipo de export mais atual

---
+++ modulos-esm/exemplos.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*Módukos ESEM: EcmaScript Moduloes (sistema, atual, moderno) */
+function saudacao(nome){ 
+    console.log("Olá"+nome+"!");
+} 
+
+
+const clientes = ["Maycon","Gabriel","Júlia"];
+
+export{saudacao, clientes};
