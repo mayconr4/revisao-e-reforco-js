899461a040ef120eb153e01b371a7165ed2abb1a
mayconr4 <mayconhs2007@gmail.com>
2025-06-09 09:21:59 -0300

    criando o pacote json

---
+++ 05-usando-modulos.js
@@ -0,0 +1,7 @@
+'use strict';
+
+//Iportação de multiplos recursos/módulos usando ESM
+import { saudacao,clientes } from "./modulos-esm/exemplos.js"; 
+
+saudacao("Fulano de tal");
+console.log(clientes);
