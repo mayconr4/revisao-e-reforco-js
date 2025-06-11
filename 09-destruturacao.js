6c373ea01ded0dfd1286f68c76a106f630575438
mayconr4 <mayconhs2007@gmail.com>
2025-06-11 08:36:01 -0300

    destruturando o array

---
+++ 09-destruturacao.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import { separador } from "./modulos-esm/exemplos.js";
+
+//Destruturando arrays
+const clientes = ["Jon oliva", "Geddy lee", "David gilmour"];
+
+const [cliente1, cliente2, cliente3] = clientes;
+
+console.log(clientes[0]);
+console.log(clientes[1]);
+console.log(clientes[2]);
